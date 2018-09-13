43a2792a512e8f38ab3587b4b210a9122c56586d
Manolis Vrondakis <mvrondakis@barracuda.com>
2018-09-13 08:04:09 +0100

    Increased zap initialisation timeout

--- src/main/java/com/barracuda/zapcomp/Constants.java
+++ src/main/java/com/barracuda/zapcomp/Constants.java
@@ -3,7 +3,7 @@ package com.barracuda.zapcomp;
 public class Constants {
     public static final int COMPLETED_PERCENTAGE = 100;
     public static final long SCAN_SLEEP = 10;
-    public static final int ZAP_INITIALIZE_TIMEOUT = 30;
+    public static final int ZAP_INITIALIZE_TIMEOUT = 100;
     public static final int ZAP_INITIALIZE_WAIT = 20;
     static final String DIRECTORY_NAME = "zap";
 }
