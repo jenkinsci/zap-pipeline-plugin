1adcb44cb8e7cf5f6973d6b88f1e4034ec97772d
bhecquet <bhecquet.w@free.fr>
2020-12-04 15:25:34 +0100

    remove unused parameter

--- src/main/java/com/vrondakis/zap/workflow/StartZapStep.java
+++ src/main/java/com/vrondakis/zap/workflow/StartZapStep.java
@@ -27,7 +27,7 @@ public class StartZapStep extends Step {
      */
     @DataBoundConstructor
     public StartZapStep(@CheckForNull String host, int port, int timeout, String zapHome, List<String> allowedHosts,
-                        String sessionPath, String zapDir) {
+                        String sessionPath) {
         zapStepParameters = new StartZapStepParameters(host, port, timeout, zapHome, allowedHosts, sessionPath);
     }
 
